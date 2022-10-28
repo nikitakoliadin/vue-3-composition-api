15926d072c03906fbb13a3013c9d0a4e43c0e8b7
Nikita Koliadin <qThegamEp@gmail.com>
2022-10-28 13:44:14 +0300

    Lesson 4

--- src/views/CapacityComposition.vue
+++ src/views/CapacityComposition.vue
@@ -1,21 +1,31 @@
 <template>
   <div>
-    <p>Capacity: {{ capacity }}</p>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
     <button @click="increaseCapacity()">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 export default {
   setup() {
     const capacity = ref(3)
+    const attending = ref(['Tim', 'Bob', 'Joe'])
 
     function increaseCapacity() {
       capacity.value++
     }
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length
+    })
 
-    return { capacity, increaseCapacity }
+    return { capacity, increaseCapacity, attending, spacesLeft }
   }
 }
 </script>
--- src/views/CapacityRegular.vue
+++ src/views/CapacityRegular.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <p>Capacity: {{ capacity }}</p>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
     <button @click="increaseCapacity()">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,12 +15,18 @@
 export default {
   data() {
     return {
-      capacity: 3
+      capacity: 3,
+      attending: ['Tim', 'Bob', 'Joe']
     }
   },
   methods: {
     increaseCapacity() {
       this.capacity++
+    }
+  },
+  computed: {
+    spacesLeft() {
+      return this.capacity - this.attending.length
     }
   }
 }
